16d56fd52a489c183dc09e3213b8c49f9e5032ea
vincent.zhang <v.zhang@aspanetconomy.net>
2020-08-30 20:27:19 +0200

    init

--- src/test/java/person/vincent/shardingsphere/ShardingsphereApplicationTests.java
+++ src/test/java/person/vincent/shardingsphere/ShardingsphereApplicationTests.java
@@ -10,6 +10,8 @@ import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.TestPropertySources;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import lombok.extern.slf4j.Slf4j;
 import person.vincent.shardingsphere.entity.Course;
 import person.vincent.shardingsphere.mapper.CourseMapper;
 
@@ -19,6 +21,7 @@ import person.vincent.shardingsphere.mapper.CourseMapper;
         @TestPropertySource("classpath:shardingsphere.properties"),
         @TestPropertySource("classpath:mybatis-plus.properties"),
 })
+@Slf4j
 class ShardingsphereApplicationTests
 {
 
@@ -26,13 +29,22 @@ class ShardingsphereApplicationTests
     private CourseMapper courseMapper;
 
     @Test
-    void addCourse()
+    public void addCourse()
     {
         Course course = new Course();
         course.setCname("name1");
         course.setCstatus("normal");
         course.setUserId(1000L);
         courseMapper.insert(course);
+    }
+
+    @Test
+    public void findCourse() {
+        QueryWrapper<Course> wrapper = new QueryWrapper<>();
+        wrapper.eq("cid", 482953882771652608L);
+
+        Course course = courseMapper.selectOne(wrapper);
+        log.info("course = {}", course);
     }
 
 }
